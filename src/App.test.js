d0ed868b888da83a5b6d28b123ba94fff511b622
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-11-22 22:42:06 +0200

    chore/setup react testing and jest dom.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,12 @@
+import '../setupTests'
+
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { render, screen } from '@testing-library/react';
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
- is not required.
-**/
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
-
-
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+    expect(screen.getByText('MyReads')).toBeInTheDocument();
+  })
+});
